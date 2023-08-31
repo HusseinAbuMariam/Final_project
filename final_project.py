d807297a8aa747c817e64388756ea50658e3ba3a
HusseinAbuMariam <huseinmohamaad@gmail.com>
2023-08-30 22:46:41 -0800

    TODO 14 and TODO 15 and TODO 16 (final)

--- final_project.py
+++ final_project.py
@@ -81,75 +81,86 @@ while True:
                               "5.Add Course to student with mark.\n"
                               "6.Exit\n"
                               "Choice: "))
-    if 1 <= selection <= 6:
-        if selection == 1:
-            student_number = input("Enter Student Number: ")
-            student_name = input("Enter Student Name: ")
-            if student_number in students:
-                print("Student number already exists. Please enter a different number.")
-            while True:
-                try:
-                    student_age = int(input("Enter Student Age: "))
-                    break
-                except:
-                    print("Invalid Value")
-        # TODO 10 make sure that Student number is not exists before
-        student_number = input("Enter Student Number: ")
-
-        student_name = input("Enter Student Name: ")
-        while True:
-            try:
-                student_age = int(input("Enter Student Age: "))
-                break
-            except:
-                print("Invalid Value")
-
-        # TODO 11 create student object and append it to students list
-        student = Student(student_number, student_name, student_age)
-        students.append(student)
-        print("Student Added Successfully")
-
-    elif selection == 2:
-        # TODO 12 find the target student using loops and delete it if exist , if not print ("Student Not Exist")
-        def find_student_index_by_number(number):
-            for index, student in enumerate(students):
-                if student_number == number:
-                    return index
-            return -1
+        if 1 <= selection <= 6:
+            if selection == 1:
+                student_number = input("Enter Student Number")
+                student_name = input("Enter Student Name")
+                if student_number in students:
+                    print("Student number already exists. Please enter a different number.")
+                while True:
+                    try:
+                        student_age = int(input("Enter Student Age"))
+                        break
+                    except:
+                        print("Invalid Value")
+                # TODO 11 create student object and append it to students list
+                student = Student(student_number, student_name, student_age)
+                students.append(student)
+                print("Student added successfully!")
+            elif selection == 2:
+                def find_student_index_by_number(number):
+                    for index, student in enumerate(students):
+                        if student_number == number:
+                            return index
+                    return -1
 
 
-        student_number = input("Enter Student Number: ")
-        student_index = find_student_index_by_number(student_number)
+                student_number = input("Enter Student Number")
+                student_index = find_student_index_by_number(student_number)
 
-        if student_index != -1:
-            del students[student_index]
-            print("Student Deleted Successfully")
+                if student_index != -1:
+                    del students[student_index]
+                    print("Student Deleted Successfully")
+                else:
+                    print("Student Not Exist")
+            elif selection == 3:
+                student_number = input("Enter Student Number")
+                # TODO 13 find the target student using loops and print student detials  if exist , if not print ("Student Not Exist")
+                student_found = False
+                for student in students:
+                    for student in students:
+                        if student_number == student_number:
+                            print("Student Details:")
+                            print(f"Number: {student_number}")
+                            print(f"Name: {student_name}")
+                            print(f"Age: {student_age}")
+                            student_found = True
+                            break
+
+                if not student_found:
+                    print("Student Not Exist")
+            elif selection == 4:
+                student_number = input("Enter Student Number: ")
+                target_student = next((student for student in students if student_number == student_number),
+                                      None)
+                if target_student:
+                    average = target_student.get_student_average()
+                    print(f"Student Average: {average}")
+                else:
+                    print("Student Not Exist")
+            elif selection == 5:
+                def find_student_index_by_number(number):
+                    for index, student in enumerate(students):
+                        if student_number == number:
+                            return index
+                    return -1
+
+
+                student_number = input("Enter Student Number: ")
+                student_index = find_student_index_by_number(student_number)
+                if student_index != -1:
+                    course_name = input("Enter Course Name: ")
+                    course_mark = float(input("Enter Course Mark: "))
+
+                    course = {"name": course_name, "mark": course_mark}
+                    students[student_index].courses_list.append(course)  # Corrected line
+                    print("Course Added Successfully")
+                else:
+                    print("Student Not Exist")
+            elif selection == 6:
+                print("Exiting the program...")
+                exit()
         else:
-            print("Student Not Exist")
-    elif selection == 3:
-        student_number = input("Enter Student Number: ")
-        # TODO 13 find the target student using loops and print student detials  if exist , if not print ("Student Not Exist")
-        student_found = False
-        for student in students:
-            for student in students:
-                if student_number == student_number:
-                    print("Student Details:")
-                    print(f"Number: {student_number}")
-                    print(f"Name: {student_name}")
-                    print(f"Age: {student_age}")
-                    student_found = True
-                    break
-
-        if not student_found:
-            print("Student Not Exist")
-    elif selection == 4:
-        student_number = input("Enter Student Number")
-        # TODO 14 find the target student using loops and get student average  if exist , if not print ("Student Not Exist")
-
-    elif selection == 5:
-        student_number = input("Enter Student Number")
-        # TODO 15 ask user to enter course name and course mark then create coures object then append it to target student courses
-
-    else:
-        # TODO 16 call a function to exit the program
-        pass
+            print("Invalid choice. Please enter a valid option.")
+    except ValueError:
+        print("Invalid input. Please enter a number.")
